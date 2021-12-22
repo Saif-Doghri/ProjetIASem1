60f73d268e03bfe5d6dd60916a383577e4b44eb4
Saif-Doghri <saif.doghri@esprit.tn>
2021-12-22 23:27:14 +0100

    Initial commit

---
+++ AssociationRules/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AssociationrulesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AssociationRules'
---
+++ DemoTechniqueML/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DemotechniquemlConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'DemoTechniqueML'
